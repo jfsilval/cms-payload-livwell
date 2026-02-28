8c0eeb85cb5dfdad59ab8058adcb5699b839ea09
jfsilval <josef.silvalopez@gmail.com>
2026-02-28 22:34:50 +0100

    Add Products collection
    Coleccion del CMS para manejar los productos

---
+++ src/collections/Products.ts
@@ -0,0 +1,61 @@
+import type { CollectionConfig } from 'payload'
+
+export const Products: CollectionConfig = {
+  slug: 'products',
+  admin: {
+    useAsTitle: 'nombre',
+  },
+  labels: {
+    singular: 'Producto',
+    plural: 'Productos',
+  },
+  fields: [
+    {
+      name: 'nombre',
+      type: 'text',
+      required: true,
+      label: 'Nombre del Producto',
+    },
+    {
+      name: 'categoria',
+      type: 'text',
+      required: true,
+      label: 'Categoría',
+    },
+    {
+      name: 'sub_categoria',
+      type: 'text',
+      label: 'Sub-Categoría',
+    },
+    {
+      name: 'clasificacion_atc',
+      type: 'text',
+      label: 'Clasificación ATC',
+    },
+    {
+      name: 'activo',
+      type: 'checkbox',
+      label: 'Activo',
+      defaultValue: true,
+    },
+    {
+      name: 'formas',
+      type: 'array',
+      label: 'Formas Farmacéuticas',
+      fields: [
+        {
+          name: 'forma_farmaceutica',
+          type: 'text',
+          required: true,
+          label: 'Forma Farmacéutica',
+        },
+        {
+          name: 'concentraciones',
+          type: 'text',
+          required: true,
+          label: 'Concentraciones',
+        },
+      ],
+    },
+  ],
+}
