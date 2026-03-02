fc6689a3b26ac0d321a6cf5b0b8fc9268dfbce81
jfsilval <josef.silvalopez@gmail.com>
2026-03-02 19:52:28 +0100

    Update index.ts to correct build mistake due to existing Collections in DB

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
 import * as migration_20260213_172621_initial from './20260213_172621_initial'
+import * as migration_20260302_181531 from './20260302_181531'
 
 export const migrations = [
   {
@@ -6,4 +7,10 @@ export const migrations = [
     down: migration_20260213_172621_initial.down,
     name: '20260213_172621_initial',
   },
+  {
+    up: migration_20260302_181531.up,
+    down: migration_20260302_181531.down,
+    name: '20260302_181531',
+  },
 ]
+
